9edc22d5c65e98a67468c036d6e3853ff817597b
Ana Luisa Marques <luisaalbernazz@gmail.com>
2025-02-03 20:48:15 -0400

    Add files via upload

--- CONSUMINDO API/index.js
+++ CONSUMINDO API/index.js
@@ -1,32 +1,32 @@
-function Procurar() {
+function Procurar(){
      let pais = document.getElementById('pais').value
-     let finalURL = 'https://restcountries.com/v3.1/name/${pais}?fullText=true'
-     console.log(finalURL)
-     fetch(finalURL)
-         .then(function (response) {
-             return response.json()
-         }
-         )
-         .then(function (data) {
+     
+let finalurl = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
+     console.log(finalurl)
+    
+     fetch (finalurl)
+    
+     .then(function(response){
+         return response.json()
+     })
+     
+    
+     .then(function(data){
          console.log(data[0])
          console.log(data[0].population)
          console.log(data[0].capital[0])
          console.log(data[0].continents[0])
- 
-         let bandeira = document.getElementsById('bandeira')
-         let nome = document.getElementsById('nome')
-         let capital = document.getElementsById('Capital')
-         let pop = document.getElementsById('Populacao')
-         let cont = document.getElementsById('Continete')
-
-         nome.innerHTML= data [0].Capital[0]
-         Cont.innerHTML= data [0].Continente[0]
-         Pop.innerHTML= data [0].Populacao
-         nome.innerHTML= data [0].capital[0]
-         bandeira.src= data [0].flags.svg 
-
- 
- 
-     }
-     )
+     
+         let bandeira = document.getElementById('bandeira')
+         let nome = document.getElementById('nome')
+         let capital= document.getElementById('capital')
+         let pop = document.getElementById('pop')
+         let cont = document.getElementById('cont')
+     
+         nome.innerHTML =pais
+         capital.innerHTML = data[0].capital[0]
+         pop.innerHTML = data[0].population
+         cont.innerHTML = data[0].continents[0]
+         bandeira .src = data[0].flags.svg
+     })
      }
